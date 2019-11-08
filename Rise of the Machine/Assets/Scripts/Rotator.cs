4205f9c8f6df375ae18b68ff02c946ee1427d58b
sag754 <56234238+sag754@users.noreply.github.com>
2019-11-08 12:43:05 -0500

    Fixed gear and ramp, fiddled with sizes.

---
+++ Rise of the Machine/Assets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public float y_speed = 1.0f;  //applies default speed and makes public for adjusting in editor/inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, y_speed, 0);  //tranform a gameobject's rotation based off the Y-axis
+
+    }
+}

eac51237f193fa7f000ff37eb4a5664182c1d131
sag754 <56234238+sag754@users.noreply.github.com>
2019-11-08 11:58:53 -0500

    Added platform 2 test ramp, Third Person Camera (Broken) and WSAD controls

---
+++ Rise of the Machine/Assets/Scripts/PlayerControllerWSAD.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerWSAD : MonoBehaviour
+{
+    Rigidbody rb; //declare a reference for the rigidbody
+
+    public float force = 100f; //create a force to push the playerObject
+    bool isSmall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //get the rigidbody from this playerObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hasInput = false; //create a local variable to track whether the user has inputed something
+
+        if (Input.GetKey(KeyCode.W))
+        { //if the W is pressed
+            rb.AddForce(new Vector3(0, 0, force)); //add force up
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        { //if the S is pressed
+            rb.AddForce(new Vector3(0, 0, -force)); //add force down
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        { //if the A is pressed
+            rb.AddForce(new Vector3(-force, 0, 0)); //add force left
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {//if the D is pressed
+            rb.AddForce(new Vector3(force, 0, 0)); //add force right
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKeyDown(KeyCode.Period)) //if > is pressed
+        {
+            if (isSmall == false) // checks if isSmall is false
+            {
+                transform.localScale -= new Vector3(2, 2, 2); //if false, make the ball smaller
+            }
+            isSmall = true; //changes isSmall to true
+        }
+
+        if (Input.GetKeyDown(KeyCode.Comma)) // if < is pressed
+        {
+            if (isSmall == true) //checks if isSmall is true
+            {
+                transform.localScale += new Vector3(2, 2, 2); //if truem make the ball bigger
+            }
+            isSmall = false; //changes isSmall to false
+        }
+
+        if (!Input.anyKey)
+        { //if the user hasn't pressed a key
+            rb.velocity = rb.velocity * 1f; //decrease velocity
+        }
+
+    }
+}
---
+++ Rise of the Machine/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour  // create Third Person Camera Class
+{
+    private const float X_ANGLE_MIN = -50.0f;  //clamps the rotation on the x to no less than -50
+    private const float X_ANGLE_MAX = 50.0f;   //clamps the rotation on the x to no more than 50
+    private const float Y_ANGLE_MIN = -3.0f;   //clamps the rotation on the y to no less than -3
+    private const float Y_ANGLE_MAX = 50.0f;   //clamps the rotation on the y to no more than 50
+
+
+    public Transform lookAt;                   //allows to set what the camera will look like
+    public Transform camTransform;             
+
+    private Camera cam;                        //references the camera
+
+    private float distance = 10.0f;            //sets the distance away from the lookAt object
+    private float currentX = 0.0f;             //sets the current X position of the camera
+    private float currentY = 0.0f;             //sets the current Y position of the camera
+    private float sensitivityX = 4.0f;         //sets how quickly the camera rotates on the X-axis
+    private float sensitivityY = 1.0f;         //sets how quickly the camera rotates on the Y-axis
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;                     // Gets the camera on the scene
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");     //sets camera control on the X-axis to the Mouse X
+        currentY -= Input.GetAxis("Mouse Y");     //sets camera control on the Y-axis to the Mouse Y
+
+        currentX = Mathf.Clamp(currentX, X_ANGLE_MIN, X_ANGLE_MAX);  //uses the float values of these variables to clamp camera position to not exceed values set on X
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);  //uses the float values of these variables to clamp camera position to not exceed values set on Y
+
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);                      //updates and changes the current camera position based on X, Y, and Z minus set distance
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);   //maintains the LookAt target as the center point for the camera
+        camTransform.position = lookAt.position + rotation * dir;        //updates the rotation and position of the camera based off the lookAt target's center point
+        camTransform.LookAt(lookAt.position);                            //updates the position of the LookAt target
+    }
+}
